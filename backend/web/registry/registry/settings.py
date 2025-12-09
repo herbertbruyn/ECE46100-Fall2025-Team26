58ef6b351642837b78e9a32599ece689a309ee65
Jamesneff <neff35@purdue.edu>
2025-12-08 20:12:26 -0500

    Unauthenticated user

--- backend/web/registry/registry/settings.py
+++ backend/web/registry/registry/settings.py
@@ -162,3 +162,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# For reset
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [],
+    'DEFAULT_PERMISSION_CLASSES': [],
+    'UNAUTHENTICATED_USER': None,
+}
