476a1a5ce538ac284677e065c077bcd6611218f7
Jamesneff <neff35@purdue.edu>
2025-12-14 15:08:04 -0500

    Revert "Updated naming convention bug"
    This reverts commit 5d9e808407df86c000628afd835b4ec8ca080ed3.

--- backend/web/registry/api/services/ingest_async_proper.py
+++ backend/web/registry/api/services/ingest_async_proper.py
@@ -300,6 +300,7 @@ class AsyncIngestService:
                     models_to_link = Artifact.objects.filter(
                         type="model",
                         code_name__icontains=artifact.name,
+                        code_name__icontains=artifact.name,
                         code__isnull=True                    
                         )
                     for model_artifact in models_to_link:
